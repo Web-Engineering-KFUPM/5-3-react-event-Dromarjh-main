627cf18f7885ac28856202370979841372eca0e1
Athar Parvez <atharparvezce@gmail.com>
2026-02-21 16:00:53 +0300

    finalising

--- 5-3-react-event-handling/src/components/TaskItem.jsx
+++ 5-3-react-event-handling/src/components/TaskItem.jsx
@@ -3,15 +3,13 @@ import React from "react";
 export default function TaskItem({ task, onDelete }) {
   return (
     <li className="taskItem">
-      {/* Task 2: show the text inside the <span> */}
-      <span>{task.text}</span>
+      {/* TODO (Task 2): show the task text inside the <span> */}
+      {/* Example: task.text */}
+      <span></span>
 
       {/* TODO (Task 3): add onClick handler to Delete button */}
-      {/* When clicked, call onDelete(id) */}
-      <button
-        className="btn btn--danger"
-        onClick={() => onDelete(task.id)}
-      >
+      {/* When clicked, call onDelete(task.id) */}
+      <button className="btn btn--danger">
         Delete
       </button>
     </li>
--- 5-3-react-event-handling/src/components/TaskList.jsx
+++ 5-3-react-event-handling/src/components/TaskList.jsx
@@ -1,4 +1,3 @@
-
 import React from "react";
 import TaskItem from "./TaskItem";
 
@@ -6,9 +5,6 @@ export default function TaskList({ tasks = [], onDelete }) {
   return (
     <ul className="list">
       {/* TODO (Task 2): if tasks.length === 0 show a placeholder message */}
-      {tasks.length === 0 && (
-        <p className="placeholder">No tasks yet. Add one above 👆</p>
-      )}
 
       {/* TODO (Task 2 & 3): map tasks to TaskItem
           Example shape:
@@ -16,13 +12,6 @@ export default function TaskList({ tasks = [], onDelete }) {
             <TaskItem key={task.id} task={task} onDelete={onDelete} />
           ))
       */}
-      {tasks.map((task) => (
-        <TaskItem
-          key={task.id}
-          task={task}
-          onDelete={onDelete}
-        />
-      ))}
     </ul>
   );
 }
